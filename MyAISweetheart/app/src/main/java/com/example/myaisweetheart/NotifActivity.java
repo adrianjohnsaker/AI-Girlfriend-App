618e1a4b6cb5c0c4a9e035f2f806851e40a7aaeb
Jonathan Marquez <45045020+AshianBoii@users.noreply.github.com>
2023-12-04 14:26:17 -0800

    Add files via upload

---
+++ MyAISweetheart/app/src/main/java/com/example/myaisweetheart/NotifActivity.java
@@ -0,0 +1,15 @@
+package com.example.myaisweetheart;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NotifActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notif_main);
+
+    }
+}
